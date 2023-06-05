e92588a1207ea788b3de9351052fb9fb7c335d0f
soonable@naver.com <soonable@naver.com>
2023-06-05 17:11:27 +0900

    Initial commit

---
+++ src/test/java/com/soon/springtx/SpringtxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soon.springtx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
